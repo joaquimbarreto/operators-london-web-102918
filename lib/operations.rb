1f975535d1b7c708128677be709246295b0dc923
Joaquim Barreto <joaquim@barreto.co>
2018-10-24 16:41:12 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed < 40 && speed > 60
-    return "unsafe"
+    return true
   else
-    return "safe"
+    return false
   end
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 && speed > 60 ? "unsafe" : "safe"
+	speed < 40 && speed > 60 ? true : false
 end
 	
 
